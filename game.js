e84eb86e79babfcd3a84dea6884be1bc960c42c9
Jaron Evans <jarondevans@gmail.com>
2019-05-20 18:22:52 -0400

    first commit

---
+++ game.js
@@ -0,0 +1,103 @@
+var Game = function() {
+  // Set the width and height of the scene.
+  this._width = 1280;
+  this._height = 720;
+
+  // Setup the rendering surface.
+  this.renderer = new PIXI.CanvasRenderer(this._width, this._height);
+  document.body.appendChild(this.renderer.view);
+
+  // Create the main stage to draw on.
+  this.stage = new PIXI.Stage();
+
+  // Start running the game.
+  this.build();
+};
+
+Game.prototype = {
+  /**
+   * Build the scene and begin animating.
+   */
+  build: function() {
+    // Draw the star-field in the background.
+    this.drawStars();
+
+    // Setup the boundaries of the game's arena.
+    this.setupBoundaries();
+    this.createShip();
+
+    //draw ship to scene
+
+    // Begin the first frame.
+    requestAnimationFrame(this.tick.bind(this));
+  },
+
+  /**
+   * Draw the field of stars behind all of the action.
+   */
+  drawStars: function() {
+    // Draw randomly positioned stars.
+    for (var i = 0; i < 1500; i++) {
+      // Generate random parameters for the stars.
+      var x = Math.round(Math.random() * this._width);
+      var y = Math.round(Math.random() * this._height);
+      var rad = Math.ceil(Math.random() * 2);
+      var alpha = Math.min(Math.random() + 0.25, 1);
+
+      // Draw the star.
+      var star = new PIXI.Graphics();
+      star.beginFill(0xffffff, alpha);
+      star.drawCircle(x, y, rad);
+      star.endFill();
+
+      // Attach the star to the stage.
+      this.stage.addChild(star);
+    }
+  },
+
+  createShip: function() {
+    this.ship = new PIXI.Graphics();
+
+    this.ship.beginFill(0x20d3fe);
+    this.ship.moveTo(0, 0);
+    this.ship.lineTo(-26, 60);
+    this.ship.lineTo(26, 60);
+    this.ship.endFill();
+
+    //add ship engine
+    this.ship.beginFill(0x1495d1);
+    this.ship.drawRect(-15, 60, 30, 8);
+    this.ship.endFill();
+
+    this.ship.x = Math.random(this._width / 2);
+    this.ship.y = Math.random(this._height / 2);
+
+    this.stage.addChild(this.ship);
+  },
+
+  /**
+   * Draw the boundaries of the space arena.
+   */
+  setupBoundaries: function() {
+    var walls = new PIXI.Graphics();
+    walls.beginFill(0xffffff, 0.5);
+    walls.drawRect(0, 0, this._width, 10);
+    walls.drawRect(this._width - 10, 10, 10, this._height - 20);
+    walls.drawRect(0, this._height - 10, this._width, 10);
+    walls.drawRect(0, 10, 10, this._height - 20);
+
+    // Attach the walls to the stage.
+    this.stage.addChild(walls);
+  },
+
+  /**
+   * Fires at the end of the gameloop to reset and redraw the canvas.
+   */
+  tick: function() {
+    // Render the stage for the current frame.
+    this.renderer.render(this.stage);
+
+    // Begin the next frame.
+    requestAnimationFrame(this.tick.bind(this));
+  }
+};
